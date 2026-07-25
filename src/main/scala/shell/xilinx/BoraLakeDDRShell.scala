1e9967ffc777581e51958326de09ed7d6cf34ed9
naichenzhao <naichenzhao13@gmail.com>
2026-07-24 17:04:48 -0700

    updated BL config

--- src/main/scala/shell/xilinx/BoraLakeDDRShell.scala
+++ src/main/scala/shell/xilinx/BoraLakeDDRShell.scala
@@ -295,20 +295,20 @@ class BoraLakeMIG(depth : BigInt)(implicit val p:Parameters) extends BlackBox
     <ReferenceClock>No Buffer</ReferenceClock>
     <SysResetPolarity>ACTIVE HIGH</SysResetPolarity>
     <BankSelectionFlag>FALSE</BankSelectionFlag>
-    <InternalVref>1</InternalVref>
+    <InternalVref>0</InternalVref>
     <dci_hr_inouts_inputs>50 Ohms</dci_hr_inouts_inputs>
     <dci_cascade>0</dci_cascade>
     <FPGADevice>
         <selected>7k/xc7k160t-fbg676</selected>
     </FPGADevice>
     <Controller number="0">
-        <MemoryDevice>DDR3_SDRAM/SODIMMs/MT8KTF51264HZ-1G6</MemoryDevice>
+        <MemoryDevice>DDR3_SDRAM/SODIMMs/MT8KTF51264HZ-1G9</MemoryDevice>
         <TimePeriod>3000</TimePeriod>
         <VccAuxIO>1.8V</VccAuxIO>
         <PHYRatio>4:1</PHYRatio>
         <InputClkFreq>166.666</InputClkFreq>
         <UIExtraClocks>0</UIExtraClocks>
-        <MMCM_VCO>666</MMCM_VCO>
+        <MMCM_VCO>800</MMCM_VCO>
         <MMCMClkOut0> 1.000</MMCMClkOut0>
         <MMCMClkOut1>1</MMCMClkOut1>
         <MMCMClkOut2>1</MMCMClkOut2>
@@ -451,11 +451,11 @@ class BoraLakeMIG(depth : BigInt)(implicit val p:Parameters) extends BlackBox
           <Pin Bank="Select Bank" PADName="No connect" name="tg_compare_error"/>
         </System_Control>
         <TimingParameters>
-          <Parameters twtr="7.5" trrd="6" trefi="7.8" tfaw="30" trtp="7.5" tcke="5" trfc="110" trp="13.75" tras="35" trcd="13.75" />
+          <Parameters tcke="5" tfaw="27" tras="34" trcd="13.91" trefi="7.8" trfc="260" trp="13.91" trrd="5" trtp="7.5" twtr="7.5"/>
         </TimingParameters>
         <mrBurstLength name="Burst Length" >8 - Fixed</mrBurstLength>
         <mrBurstType name="Read Burst Type and Length" >Sequential</mrBurstType>
-        <mrCasLatency name="CAS Latency" >11</mrCasLatency>
+        <mrCasLatency name="CAS Latency" >6</mrCasLatency>
         <mrMode name="Mode" >Normal</mrMode>
         <mrDllReset name="DLL Reset" >No</mrDllReset>
         <mrPdMode name="DLL control for precharge PD" >Slow Exit</mrPdMode>
